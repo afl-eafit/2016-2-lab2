4f77069b4b3a55df0a7dbccaff97c044eb316d5d
acalles1 <Alejandro.Calle434@gmail.com>
2016-10-04 15:24:17 -0500

    Changed some things in examples

--- Examples.hs
+++ Examples.hs
@@ -15,7 +15,7 @@ universe1, universe2 :: RE
 universe1 = Star (Plus _0 _1)
 universe2 = Star (Plus _1 _0)
 
-s :: RE
+s, r :: RE
 s = universe1 `Dot` _1
 r = Plus (Plus _0 _1) universe1
 
@@ -23,24 +23,19 @@ r = Plus (Plus _0 _1) universe1
 --simplification
 
 m1 :: ENFA Int Char
-m1 = ENFA (fromList [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+m1 = ENFA (fromList [0, 1, 2, 3])
         (fromList ['0','1','e'])
         deltaM
         (0)
-        (singleton 11)
+        (singleton 3)
     where
       deltaM :: Int -> Char -> Set Int
-      deltaM 0  'e' = fromList [1,2,3,11]
-      deltaM 1  '1' = singleton 4
-      deltaM 2  '0' = singleton 5
-      deltaM 3  'e' = fromList [6, 7]
-      deltaM 4  'e' = singleton 11
-      deltaM 5  'e' = singleton 11
-      deltaM 6  '1' = singleton 8
-      deltaM 7  '0' = singleton 9
-      deltaM 8  'e' = singleton 10
-      deltaM 9  'e' = singleton 10
-      deltaM 10 'e' = fromList [3, 11]
+      deltaM 0  'e' = fromList [1, 3]
+      deltaM 0  '0' = singleton 3
+      deltaM 0  '1' = singleton 3
+      deltaM 1  '0' = singleton 2
+      deltaM 1  '1' = singleton 2
+      deltaM 2  'e' = fromList [1,3]
       deltaM  _  _  = empty
 
 --The regular expression s yields an e-nfa like this using 2nd
@@ -63,25 +58,24 @@ m2 = ENFA (fromList [0, 1, 2, 3, 4, 5, 6, 7, 8])
       deltaM 6 'e' = singleton 7
       deltaM 7 'e' = singleton 1
       deltaM 7 '1' = singleton 8
-      deltaM  _  _  = empty
+      deltaM _  _  = empty
 
 --The regular expression s yields an e-nfa like this using 3rd
 --simplifcation
 
 m3 :: ENFA Int Char
-m3 = ENFA (fromList [0, 1, 2, 3, 4, 5, 6, 7, 8])
+m3 = ENFA (fromList [0, 1, 2, 3, 4, 5, 6, 7])
         (fromList ['0','1','e'])
         deltaM
         (0)
         (singleton 8)
     where
       deltaM :: Int -> Char -> Set Int
-      deltaM 0 'e' = fromList [1,7]
-      deltaM 1 'e' = fromList [2,3]
-      deltaM 2 '1' = singleton 4
-      deltaM 3 '0' = singleton 5
-      deltaM 4 'e' = singleton 6
-      deltaM 5 'e' = singleton 6
-      deltaM 6 'e' = singleton 7
-      deltaM 7 '1' = singleton 8
+      deltaM 0 'e' = fromList [1, 2, 7]
+      deltaM 1 '1' = singleton 1
+      deltaM 2 'e' = singleton 5
+      deltaM 3 '0' = singleton 4
+      deltaM 4 'e' = singleton 5
+      deltaM 5 'e' = fromList [0, 6]
+      deltaM 6 '1' = singleton 7
       deltaM  _  _  = empty
