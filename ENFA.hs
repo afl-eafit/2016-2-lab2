eb4fa434e087647fff2b6247f9056713f33d8f7a
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2016-10-07 15:35:52 -0500

    Fixed layout.

--- ENFA.hs
+++ ENFA.hs
@@ -4,9 +4,9 @@ import Data.Set ( Set )
 
 -- | A non-deterministic finite automaton with epsilon transitions.
 data ENFA state symbol =
-  ENFA { states      :: Set state                    --  States
-        , symbols    :: Set symbol                   --  Input symbols
-        , delta      :: state -> symbol -> Set state --  Transition function
-        , start      :: state                        --  Start state
-        , accepting  :: Set state                    --  Accepting states
-        }
+  ENFA { states     :: Set state                     --  States
+       , symbols    :: Set symbol                    --  Input symbols
+       , delta      :: state -> symbol -> Set state  --  Transition function
+       , start      :: state                         --  Start state
+       , accepting  :: Set state                     --  Accepting states
+       }
